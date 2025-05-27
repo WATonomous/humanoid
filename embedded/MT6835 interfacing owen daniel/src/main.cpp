f7823e469c06c2dcae91d9aa83cb8e3c9fed453c
Daniel <danielxie0713@gmail.com>
2025-05-27 19:35:39 -0400

    added libraries

--- embedded/MT6835 interfacing owen daniel/src/main.cpp
+++ embedded/MT6835 interfacing owen daniel/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "SimpleFOC.h"
+#include "SimpleFOCDrivers.h"
+
+#include "encoders/mt6835/MagneticSensorMT6835.h"
+
+#define SENSOR_nCS PB6
+
+SPISettings myMT6835SPISettings(1000000, MT6835_BITORDER, SPI_MODE3);
+MagneticSensorMT6835 sensor = MagneticSensorMT6835(SENSOR_nCS, myMT6835SPISettings);
+
+long ts;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    sensor.init();
+    ts = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    sensor.update();
+    long now = millis();
+    if (now - ts > 1000) {
+        ts = now;
+        SimpleFOCDebug::print("A: ");
+        SimpleFOCDebug::print(sensor.getAngle());
+        SimpleFOCDebug::print(" V: ");
+        SimpleFOCDebug::println(sensor.getVelocity());
+    }
+    delay(10);
 }
