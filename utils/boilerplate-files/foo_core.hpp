245629e97fde1e0d6e2b39e67c82e59a6b721b1c
Melika Mir Sajjadi <msmel@Melikas-MacBook-Air.local>
2025-06-07 15:47:52 -0400

    boilerplate files created

---
+++ utils/boilerplate-files/foo_core.hpp
@@ -0,0 +1,17 @@
+#ifndef FOO_CORE_HPP_
+#define FOO_CORE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+
+class FooCore {
+public:
+  /*
+   * Foo core constructor.
+   */
+  FooCore();
+
+private:
+  
+};
+
+#endif
