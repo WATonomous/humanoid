5ff13d9ab7a394e4c4b50c81aa4aaafb97455bd2
Yichen Liu <yichen.liu@hotmail.com>
2025-06-14 17:32:32 -0400

    Added example rosbridge publisher node

---
+++ autonomy/teleop/rosbridge_publisher/include/rosbridge_publisher_node.hpp
@@ -0,0 +1,31 @@
+#ifndef ROSBRIDGE_PUBLISHER_NODE_HPP_
+#define ROSBRIDGE_PUBLISHER_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "sample_msgs/msg/hand_pose.hpp"
+
+#include "rosbridge_publisher_core.hpp"
+
+/**
+ * Implementation of a ROS2 node that listens to the "unfiltered" and "filtered"
+ * topics and echoes the operating frequency of the topic to the console.
+ */
+class ROSbridgePublisherNode: public rclcpp::Node {
+public:
+  
+    ROSbridgePublisherNode();
+
+private:
+
+    void timer_callback();
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    rclcpp::Publisher<sample_msgs::msg::HandPose>::SharedPtr hand_pose_pub_;
+
+    ROSbridgePublisherCore rosbridge_publisher_;
+
+};
+
+#endif
