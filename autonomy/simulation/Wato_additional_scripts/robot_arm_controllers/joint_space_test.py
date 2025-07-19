a088db801882372dd51959fe26129a65186d87a6
wilsonchenghy <wilsonchenghy@gmail.com>
2025-07-19 16:55:54 -0400

    Revert "Merge pull request #18 from WATonomous/fix-add-simulation-kinematics-scripts"
    This reverts commit c3ad9b4f5d590efa7632dc0bc56cd919f814c7dc, reversing
    changes made to 5be2775a49c327d006f5198ece9df630e95d058b.

--- autonomy/simulation/Wato_additional_scripts/robot_arm_controllers/joint_space_test.py
+++ autonomy/simulation/Wato_additional_scripts/robot_arm_controllers/joint_space_test.py
@@ -1,30 +1,30 @@
-from isaaclab_assets import UR10_CFG
-from isaaclab.utils.math import subtract_frame_transforms
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
-from isaaclab.utils import configclass
-from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
-from isaaclab.markers.config import FRAME_MARKER_CFG
-from isaaclab.markers import VisualizationMarkers
-from isaaclab.managers import SceneEntityCfg
-from isaaclab.controllers import DifferentialIKController, DifferentialIKControllerCfg
-from isaaclab.assets import AssetBaseCfg
-import isaaclab.sim as sim_utils
-import torch
 import argparse
 from isaaclab.app import AppLauncher
 
-parser = argparse.ArgumentParser(
-    description="Robot Arm with Joint Space Control")
+parser = argparse.ArgumentParser(description="Robot Arm with Joint Space Control")
 AppLauncher.add_app_launcher_args(parser)
 args_cli = parser.parse_args()
 
 app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
+import torch
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import AssetBaseCfg
+from isaaclab.controllers import DifferentialIKController, DifferentialIKControllerCfg
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.markers import VisualizationMarkers
+from isaaclab.markers.config import FRAME_MARKER_CFG
+from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
+from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.utils.math import subtract_frame_transforms
 
 ##
 # Pre-defined configs
 ##
+from isaaclab_assets import UR10_CFG
 
 
 @configclass
@@ -41,22 +41,15 @@ class TableTopSceneCfg(InteractiveSceneCfg):
     # lights
     dome_light = AssetBaseCfg(
         prim_path="/World/Light",
-        spawn=sim_utils.DomeLightCfg(
-            intensity=3000.0,
-            color=(
-                0.75,
-                0.75,
-                0.75)))
+        spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
 
     # mount
     table = AssetBaseCfg(
         prim_path="{ENV_REGEX_NS}/Table",
         spawn=sim_utils.UsdFileCfg(
-            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd",
-            scale=(
-                2.0,
-                2.0,
-                2.0)),
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
+        ),
     )
 
     # articulation
@@ -64,32 +57,25 @@ class TableTopSceneCfg(InteractiveSceneCfg):
 
 
 def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
-
+    
     robot = scene["robot"]
 
     # Create controller
-    diff_ik_cfg = DifferentialIKControllerCfg(
-        command_type="pose", use_relative_mode=False, ik_method="dls")
-    diff_ik_controller = DifferentialIKController(
-        diff_ik_cfg, num_envs=scene.num_envs, device=sim.device)
+    diff_ik_cfg = DifferentialIKControllerCfg(command_type="pose", use_relative_mode=False, ik_method="dls")
+    diff_ik_controller = DifferentialIKController(diff_ik_cfg, num_envs=scene.num_envs, device=sim.device)
 
     # Markers
     frame_marker_cfg = FRAME_MARKER_CFG.copy()
     frame_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
-    ee_marker = VisualizationMarkers(
-        frame_marker_cfg.replace(
-            prim_path="/Visuals/ee_current"))
+    ee_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/ee_current"))
 
     # Specify robot-specific parameters
-    robot_entity_cfg = SceneEntityCfg(
-        "robot", joint_names=[".*"], body_names=["ee_link"])
+    robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*"], body_names=["ee_link"])
 
     # Resolving the scene entities
     robot_entity_cfg.resolve(scene)
 
-    # Obtain the frame index of the end-effector ; For a fixed base robot, the
-    # frame index is one less than the body index. This is because ; the root
-    # body is not included in the returned Jacobians.
+    # Obtain the frame index of the end-effector ; For a fixed base robot, the frame index is one less than the body index. This is because ; the root body is not included in the returned Jacobians.
     if robot.is_fixed_base:
         ee_jacobi_idx = robot_entity_cfg.body_ids[0] - 1
     else:
@@ -113,24 +99,20 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
                 [1.0, -1.712, 1.712, 0.0, 0.0, 0.0],
                 [0.0, 1.712, 1.712, 0.0, 0.0, 0.0],
             ]
-            joint_position = torch.tensor(
-                joint_position_list[joint_position_index],
-                device=sim.device)
+            joint_position = torch.tensor(joint_position_list[joint_position_index], device=sim.device)
             joint_vel = robot.data.default_joint_vel.clone()
 
             if joint_position_index >= len(joint_position_list) - 1:
                 joint_position_index = 0
             else:
                 joint_position_index += 1
-
+                
             robot.reset()
 
-            joint_pos_des = joint_position.unsqueeze(
-                0)[:, robot_entity_cfg.joint_ids].clone()
+            joint_pos_des = joint_position.unsqueeze(0)[:, robot_entity_cfg.joint_ids].clone()
 
         # apply actions (Smooth movement)
-        robot.set_joint_position_target(
-            joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
+        robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
         scene.write_data_to_sim()
 
         # perform step
@@ -143,8 +125,7 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
         scene.update(sim_dt)
 
         # obtain quantities from simulation
-        ee_pose_w = robot.data.body_state_w[:,
-                                            robot_entity_cfg.body_ids[0], 0:7]
+        ee_pose_w = robot.data.body_state_w[:, robot_entity_cfg.body_ids[0], 0:7]
         # update marker positions
         ee_marker.visualize(ee_pose_w[:, 0:3], ee_pose_w[:, 3:7])
 
