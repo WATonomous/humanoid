735da1aced134cce4b9165cc1eefe32bcafb8572
WATonomousAdmin <admin@watonomous.ca>
2025-07-23 00:37:03 +0000

    Fix code style issues with clang_format

--- autonomy/teleop/rosbridge_publisher/src/rosbridge_publisher_core.cpp
+++ autonomy/teleop/rosbridge_publisher/src/rosbridge_publisher_core.cpp
@@ -3,14 +3,14 @@
 
 #include "rosbridge_publisher_core.hpp"
 
-ROSbridgePublisherCore::ROSbridgePublisherCore(){}
+ROSbridgePublisherCore::ROSbridgePublisherCore() {}
 
-std::string ROSbridgePublisherCore::hand_pose_to_string(const sample_msgs::msg::HandPose pose)
-{
+std::string ROSbridgePublisherCore::hand_pose_to_string(
+    const sample_msgs::msg::HandPose pose) {
   std::string s;
   std::vector<float> hand_pose = pose.data;
   s += "[";
-  for(int i=0; i < hand_pose.size(); i++){
+  for (int i = 0; i < hand_pose.size(); i++) {
     s += std::to_string(hand_pose[i]);
     s += ",";
   }
