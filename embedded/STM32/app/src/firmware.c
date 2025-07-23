05bc0eea7409e031af26b8bb8404196ba984d803
Ella Lewis <ella.shan@computingstack.com>
2025-07-22 20:51:30 -0400

    Setting up serial monitor configuration

--- embedded/STM32/app/src/firmware.c
+++ embedded/STM32/app/src/firmware.c
@@ -6,6 +6,7 @@
 #include "task.h"
 
 #define BOOTLOADER_SIZE (0x08008000U)
+UART_HandleTypeDef huart1;
 
 // set the vector table offset of app firmware image
 static void vector_setup(void)
@@ -29,8 +30,11 @@ void stm_to_esp()
     {
         uint8_t rx_buf[32] = {0};
         HAL_StatusTypeDef status;
+        const char debug_msg[] = "Debug\r\n";
+        HAL_UART_Transmit(&huart1, (uint8_t *)debug_msg, strlen(debug_msg), HAL_MAX_DELAY);
 
         // wait until data is received (blocking call)
+
         status = HAL_UART_Receive(&huart1, rx_buf, sizeof(rx_buf) - 1, HAL_MAX_DELAY);
         if (status == HAL_OK)
         {
@@ -38,8 +42,50 @@ void stm_to_esp()
             HAL_UART_Transmit(&huart1, rx_buf, strlen((char *)rx_buf), HAL_MAX_DELAY);
         }
     }
-};
+}
 
+static void MX_USART1_UART_Init(void)
+{
+
+    /* USER CODE BEGIN USART1_Init 0 */
+
+    /* USER CODE END USART1_Init 0 */
+
+    /* USER CODE BEGIN USART1_Init 1 */
+
+    /* USER CODE END USART1_Init 1 */
+
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+    huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN USART1_Init 2 */
+
+    /* USER CODE END USART1_Init 2 */
+}
 int main()
 {
     vector_setup();
@@ -57,53 +103,15 @@ int main()
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     // create blinking led task
-    xTaskCreate(blink_led, "BLINK_LED", 128, NULL, 1, NULL);
+    // // xTaskCreate(blink_led, "BLINK_LED", 128, NULL, 1, NULL);
+    // xTaskCreate(stm_to_esp, "STM_TO_ESP", 128, NULL, 1, NULL);
+    const char debug_msg[] = "Debug\r\n";
+    HAL_UART_Transmit(&huart1, (uint8_t *)debug_msg, strlen(debug_msg), HAL_MAX_DELAY);
 
     // start FreeRTOS Scheduler
     vTaskStartScheduler();
 
     while (1)
-        ;
-
-    static void MX_USART1_UART_Init(void)
     {
-
-        /* USER CODE BEGIN USART1_Init 0 */
-
-        /* USER CODE END USART1_Init 0 */
-
-        /* USER CODE BEGIN USART1_Init 1 */
-
-        /* USER CODE END USART1_Init 1 */
-        huart1.Instance = USART1;
-        huart1.Init.BaudRate = 115200;
-        huart1.Init.WordLength = UART_WORDLENGTH_8B;
-        huart1.Init.StopBits = UART_STOPBITS_1;
-        huart1.Init.Parity = UART_PARITY_NONE;
-        huart1.Init.Mode = UART_MODE_TX_RX;
-        huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-        huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-        huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-        huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-        huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-        if (HAL_UART_Init(&huart1) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        /* USER CODE BEGIN USART1_Init 2 */
-
-        /* USER CODE END USART1_Init 2 */
     }
 }
