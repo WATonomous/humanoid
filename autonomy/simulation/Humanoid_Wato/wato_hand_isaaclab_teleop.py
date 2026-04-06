4198c8fdf97d4136bd2e5a00381d67656ce0900b
InfiniteParticles <64912490+RijulChaddha9504@users.noreply.github.com>
2026-04-05 23:07:56 -0600

    Arm motion

--- autonomy/simulation/Humanoid_Wato/wato_hand_isaaclab_teleop.py
+++ autonomy/simulation/Humanoid_Wato/wato_hand_isaaclab_teleop.py
@@ -90,7 +90,7 @@ class ArmHandSceneCfg(InteractiveSceneCfg):
             ),
         ),
         init_state=ArticulationCfg.InitialStateCfg(
-        pos=(-0.5, 0.9, 0.0),
+        pos=(-0.3, 0.9, 0.0),
         rot=(0.707, 0.0, 0.0, -0.707),
         joint_pos={
             "door_left_joint":    0.0,
