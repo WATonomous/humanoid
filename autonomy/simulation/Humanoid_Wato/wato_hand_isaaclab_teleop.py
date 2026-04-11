63824a60b3ef341fb770d19467d712e7c1151dff
InfiniteParticles <64912490+RijulChaddha9504@users.noreply.github.com>
2026-04-11 00:59:47 -0600

    Arm motion

--- autonomy/simulation/Humanoid_Wato/wato_hand_isaaclab_teleop.py
+++ autonomy/simulation/Humanoid_Wato/wato_hand_isaaclab_teleop.py
@@ -546,7 +546,7 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
         surface_dist = abs(dist_to_plane)
 
         print(f"[SURFACE] dist={surface_dist:.3f}")
-        SURFACE_THRESH = 0.06  # 6 cm
+        SURFACE_THRESH = 0.17  # 17 cm
         is_touching_surface = surface_dist < SURFACE_THRESH
         is_in_front = dist_to_plane > 0
 
