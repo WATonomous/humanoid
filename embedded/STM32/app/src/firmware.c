bdf8975a0ad12e6312f39de58031ebfd15d9fef7
Ramy Wahib <ramy.wahib99@gmail.com>
2026-05-13 04:43:55 -0400

    removing embedded

--- embedded/STM32/app/src/firmware.c
+++ embedded/STM32/app/src/firmware.c
@@ -11,40 +11,35 @@
 static void vector_setup(void) { SCB->VTOR = BOOTLOADER_SIZE; }
 
 // thread for blinking led
-void blink_led(void *pvParams)
-{
-    while (1)
-    {
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        // vTaskDelay(pdMS_TO_TICKS(500));
-        HAL_Delay(500);
-    }
+void blink_led(void *pvParams) {
+  while (1) {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    // vTaskDelay(pdMS_TO_TICKS(500));
+    HAL_Delay(500);
+  }
 }
 
-int main()
-{
-    vector_setup();
-    HAL_Init();
-    system_setup();
+int main() {
+  vector_setup();
+  HAL_Init();
+  system_setup();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    // Configure GPIO PIN5;
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = GPIO_PIN_5;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  // Configure GPIO PIN5;
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    // create blinking led task
-    can_rx_queue = xQueueCreate(10, sizeof(CAN_RxMessage_t));
-    xTaskCreate(blink_led, "BLINK_LED", 128, NULL, 1, NULL);
-    xTaskCreate(can_rx_task, "CAN_RX", 256, NULL, 2, NULL);
+  // create blinking led task
+  xTaskCreate(blink_led, "BLINK_LED", 128, NULL, 1, NULL);
 
-    // start FreeRTOS Scheduler
-    vTaskStartScheduler();
+  // start FreeRTOS Scheduler
+  vTaskStartScheduler();
 
-    while (1)
-        ;
+  while (1)
+    ;
 }
