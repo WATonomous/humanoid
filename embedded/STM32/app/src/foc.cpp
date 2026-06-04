6e8643a08c19bb2714bdf3088bbc9bafee8b807b
BranavanK <60591247+Brxnxvxn@users.noreply.github.com>
2026-06-03 20:22:09 -0400

    FOC and ring buffer impl (#65)

--- embedded/STM32/app/src/foc.cpp
+++ embedded/STM32/app/src/foc.cpp
@@ -1,5 +1,9 @@
 
+<<<<<<< HEAD
 /** For setup
+=======
+/** For setup 
+>>>>>>> d0b14223 (FOC and ring buffer impl (#65))
  * - initialize PWM
  * - initialize angle/position encoder (MT6835)
  * - intialize current sensing
@@ -7,13 +11,34 @@
  * - intialize motor configurations (PID, mode) + motor.initFOC()
  * - intialize CANFD and enable RX ISR
  *
+<<<<<<< HEAD
  */
 void setup() {}
+=======
+*/
+void setup()
+{
+
+
+
+}
+
+>>>>>>> d0b14223 (FOC and ring buffer impl (#65))
 
 /** For loop
  *  -call loopFOC, move
  *  -check ringbuffer for messages
  *  -send telemetry data back
+<<<<<<< HEAD
  *
  */
 void loop() {}
+=======
+ * 
+ */
+void loop()
+{
+
+
+}
+>>>>>>> d0b14223 (FOC and ring buffer impl (#65))
--- embedded/STM32/src/Drivers/ring_buffer.c
+++ embedded/STM32/src/Drivers/ring_buffer.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "ring_buffer.h"
 
 void ring_buffer_init(ring_buffer_t* rb, uint8_t* buf, uint32_t size) {
@@ -36,3 +37,39 @@ bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte) {
 
   return true;
 }
+=======
+#include "../../app/inc/common_defines.h"
+
+#define RING_BUF_SIZE   128U
+
+typedef struct ring_buffer_s
+{
+    uint32_t read_index;
+    uint32_t write_index;
+    uint8_t* buf;
+    uint32_t size;
+} ring_buffer_t;
+
+void ring_buffer_init(ring_buffer_t* rb, uint8_t* buf, uint32_t size)
+{
+
+
+}
+
+bool ring_buffer_read(ring_buffer_t* rb, uint8_t* byte)
+{
+
+
+}
+
+bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte)
+{
+
+
+}
+
+bool ring_buffer_empty(ring_buffer_t* rb)
+{
+
+}
+>>>>>>> d0b14223 (FOC and ring buffer impl (#65))
