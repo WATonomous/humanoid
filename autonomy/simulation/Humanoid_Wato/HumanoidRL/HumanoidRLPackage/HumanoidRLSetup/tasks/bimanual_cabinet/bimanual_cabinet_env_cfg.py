b749f950ea9366081cea90e2561719b042c7a9c1
InfiniteParticles <64912490+RijulChaddha9504@users.noreply.github.com>
2026-06-08 01:49:49 -0400

    Run script

--- autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/HumanoidRLSetup/tasks/bimanual_cabinet/bimanual_cabinet_env_cfg.py
+++ autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/HumanoidRLSetup/tasks/bimanual_cabinet/bimanual_cabinet_env_cfg.py
@@ -91,7 +91,7 @@ class CabinetSceneCfg(InteractiveSceneCfg):
                 prim_path="{ENV_REGEX_NS}/Cabinet/drawer_handle_top",
                 name="drawer_handle_top",
                 offset=OffsetCfg(
-                    pos=(0.305, 0.0, 0.01),
+                    pos=(0.39, 0.0, 0.01),
                     rot=(0.5, 0.5, -0.5, -0.5),  # align with end-effector frame
                 ),
             ),
