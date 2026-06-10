0f09af1523f1c8a19266a71a2c0fbdf6f3ea49bd
woojaek <w39kim@gmail.com>
2026-06-10 19:37:02 -0400

    Implement ring_buffer_read function

--- embedded/STM32/src/Drivers/ring_buffer.c
+++ embedded/STM32/src/Drivers/ring_buffer.c
@@ -1,34 +1,25 @@
 #include "../../app/inc/common_defines.h"
 
-#define RING_BUF_SIZE   128U
+#define RING_BUF_SIZE 128U
 
-typedef struct ring_buffer_s
-{
-    uint32_t read_index;
-    uint32_t write_index;
-    uint8_t* buf;
-    uint32_t size;
+typedef struct ring_buffer_s {
+  uint32_t read_index;
+  uint32_t write_index;
+  uint8_t* buf;
+  uint32_t size;
 } ring_buffer_t;
 
-void ring_buffer_init(ring_buffer_t* rb, uint8_t* buf, uint32_t size)
-{
+void ring_buffer_init(ring_buffer_t* rb, uint8_t* buf, uint32_t size) {}
 
-
+bool ring_buffer_read(ring_buffer_t* rb, uint8_t* byte) {
+  if (ring_buffer_empty(rb)) {
+    return false;
+  }
+  *byte = rb->buf[rb->read_index];
+  rb->read_index = (rb->read_index + 1) & (rb->size - 1);
+  return true;
 }
 
-bool ring_buffer_read(ring_buffer_t* rb, uint8_t* byte)
-{
+bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte) {}
 
-
-}
-
-bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte)
-{
-
-
-}
-
-bool ring_buffer_empty(ring_buffer_t* rb)
-{
-
-}
+bool ring_buffer_empty(ring_buffer_t* rb) {}
