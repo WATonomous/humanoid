83bd847202514c4e3429d86012ea16302519b709
Hany25 <YitingHan4@gmail.com>
2026-06-21 10:41:57 -0400

    initial values

--- embedded/STM32/app/src/foc.cpp
+++ embedded/STM32/app/src/foc.cpp
@@ -8,10 +8,46 @@
  * - intialize CANFD and enable RX ISR
  *
 */
+
+#include <SimpleFOC.h>
+
+BLDCMotor motor = BLDCMotor(7);
+
+// enable pin might need to be changed
+BLDCDriver3PWM driver = BLDCDriver3PWM(PA_8, PA_9, PA_10, PB_5);
+
 void setup()
 {
 
+  Serial.begin(115200);
 
+  SimpleFOCDebug::enable(&Serial);
+  
+  // need to check with motor driver specs
+  driver.voltage_power_supply = 12.0;
+  // used for initial testing
+  driver.voltage_limit = 2.0;
+  // might need to be changed
+  driver.pwm_frequency = 20000;
+
+  // driver init
+  if (!driver.init()){
+    Serial.println("Driver init failed!");
+    return;
+  }
+
+  // enable driver
+  driver.enable();
+  motor.linkDriver(&driver);
+
+  motor.voltage_limit = 2.0;
+  motor.controller = MotionControlType::torque;
+  motor.torque_controller = TorqueControlType::voltage;
+
+  motor.init();
+
+  Serial.println("PWM, driver, and motor initialized.");
+  _delay(1000);
 
 }
 
@@ -24,6 +60,5 @@ void setup()
  */
 void loop()
 {
-
 
 }
