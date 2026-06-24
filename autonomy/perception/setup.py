36278a95e83551e81450ff6a2d4999f719c298be
Allen Abraham <allenabraham106@gmail.com>
2026-06-24 16:03:42 -0400

    Added RTMPose-based pose estimation node

--- autonomy/perception/setup.py
+++ autonomy/perception/setup.py
@@ -6,31 +6,35 @@ package_name = 'perception'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
         # Install marker file in the package index
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         # Include our package.xml file
-        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join("share", package_name), ["package.xml"]),
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'),
-         glob(os.path.join('launch', '*.launch.py'))),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.launch.py")),
+        ),
         # Include all config files.
-        (os.path.join('share', package_name, 'config'),
-         glob(os.path.join('config', '*.yaml'))),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='watonomous',
-    maintainer_email='watonomous@uwaterloo.ca',
-    description='Perception pipeline for humanoid robot',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="watonomous",
+    maintainer_email="watonomous@uwaterloo.ca",
+    description="Perception pipeline for humanoid robot",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'dummy_publisher_node = perception.dummy_publisher_node:main'
+        "console_scripts": [
+            "dummy_publisher_node = perception.dummy_publisher_node:main",
+            "pose_estimation_node = perception.pose_estimation_node:main"
         ],
     },
 )
