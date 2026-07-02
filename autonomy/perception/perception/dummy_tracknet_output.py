3d43f39752df8a12a9ea6c19086be065e3e16e87
IdkWhatUsername2do <szlgm2018@gmail.com>
2026-07-02 01:11:37 -0400

    Seeded velocity with linear approximation in beginning

--- autonomy/perception/perception/dummy_tracknet_output.py
+++ autonomy/perception/perception/dummy_tracknet_output.py
@@ -65,7 +65,7 @@ class DummyTrackNetOutput(Node):
 
         noisy_dir = noisy_dir / np.linalg.norm(noisy_dir)
 
-        velocity = np.random.uniform(70, 130) * noisy_dir
+        velocity = np.random.uniform(70,130) * noisy_dir
 
         self.shuttle = Shuttle(velocity, position)
     
