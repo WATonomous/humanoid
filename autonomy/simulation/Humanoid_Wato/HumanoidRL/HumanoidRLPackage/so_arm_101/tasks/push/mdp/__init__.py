8f8f3f9af04c5f86d5e3b4496bcd9674df84da87
Johnson Yep <johnsonyep@hotmail.com>
2026-07-07 23:28:04 -0400

    Added push block task for so_arm_101

---
+++ autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/so_arm_101/tasks/push/mdp/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2024-2025, Muammer Bay (LycheeAI), Louis Le Lay
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""MDP terms for the push-block task.
+
+Re-exports the standard Isaac Lab manipulation terms (actions, generic
+observations, events, ...) and adds the push-specific terms.
+"""
+
+from isaaclab_tasks.manager_based.manipulation.lift.mdp import *  # noqa: F401, F403
+
+from .curriculums import *  # noqa: F401, F403
+from .events import *  # noqa: F401, F403
+from .observations import *  # noqa: F401, F403
+from .rewards import *  # noqa: F401, F403
+from .terminations import *  # noqa: F401, F403
+from .utils import BLOCK_HALF_SIZE, block_center_w  # noqa: F401
