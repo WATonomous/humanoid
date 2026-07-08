888dcb4dccb018292408b9faa1e0900dc75411a1
Johnson Yep <johnsonyep@hotmail.com>
2026-07-08 00:23:28 -0400

    removed copyright headers

--- autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/so_arm_101/tasks/push/mdp/__init__.py
+++ autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/so_arm_101/tasks/push/mdp/__init__.py
@@ -1,13 +1,3 @@
-# Copyright (c) 2024-2025, Muammer Bay (LycheeAI), Louis Le Lay
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 """MDP terms for the push-block task.
 
 Re-exports the standard Isaac Lab manipulation terms (actions, generic
--- autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/so_arm_101/tasks/push/mdp/utils.py
+++ autonomy/simulation/Humanoid_Wato/HumanoidRL/HumanoidRLPackage/so_arm_101/tasks/push/mdp/utils.py
@@ -1,13 +1,3 @@
-# Copyright (c) 2024-2025, Muammer Bay (LycheeAI), Louis Le Lay
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 """Shared helpers for the push-block MDP terms."""
 
 from __future__ import annotations
