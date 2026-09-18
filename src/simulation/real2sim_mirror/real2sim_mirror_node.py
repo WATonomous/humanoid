4c7a18bba130a09728ff2722898af7f2b78b2696
nitish2006main <n4reddy@uwaterloo.ca>
2026-09-17 22:37:22 -0400

    Added MJlabs setup for the real2sim mirror node. Visualization script is almost done. Task 280

--- src/simulation/real2sim_mirror/real2sim_mirror_node.py
+++ src/simulation/real2sim_mirror/real2sim_mirror_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from common_msgs.msg import MotorFeedback
 from config_loader import load_hardware_mapping, angle_computation
+import mujoco
 
 class Real2SimMirrorNode(Node):
     def __init__(self):
@@ -14,14 +15,37 @@ class Real2SimMirrorNode(Node):
         )
         self.lookup_table = load_hardware_mapping("src/joint_command/config/hardware_mapping.yaml")
 
+        self.model = mujoco.MjModel.from_xml_path("src/simulation/real2sim_mirror/mirror.xml")
+        self.data = mujoco.MjData(self.model)
+
     def feedback_callback(self, msg):
         motor_id = msg.motor_id
         position = msg.position
-        angle = angle_computation(motor_id, position, self.lookup_table)
-        print(f"{msg.motor_id}: {angle} rad")
+
+        angle_rad = angle_computation(motor_id, position, self.lookup_table)
+        
+        if angle_rad is None:
+            return
+        
+        joint_name = self.lookup_table[motor_id]["joint_name"]
+        joint_id = mujoco.mj_name2id(
+            self.model,
+            mujoco.mjtObj.mjOBJ_JOINT,
+            joint_name
+        )
+
+        qpos_addr = self.model.jnt.qposadr[joint_id]
+        self.data.qpos[qpos_addr] = angle_rad
+
+        mujoco.mj_forward(self.model, self.data)
+
 
 if __name__ == "__main__":
     rclpy.init()
     node = Real2SimMirrorNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
+
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
